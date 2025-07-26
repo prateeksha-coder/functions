36891d395fe5deedd1ee7023c558f315363d43ed
prateeksha-coder <prateeksha.codingal@gmail.com>
2025-07-26 12:00:15 +0530

    commit

--- bil.py
+++ bil.py
@@ -1,6 +1,12 @@
-def func():
-    #'''nsbcnmdbmnb'''
-    return None
 
-print("Using __doc__:")
-print(func.__doc__)
+def greet(name,message="Hi"):
+    return f"{message} {name}"
+
+n=greet("Anmol","Hello")
+print(n)
+
+def subtract(x,y):
+    ''' This function is about subtracting and positional arguments'''
+    return x-y
+print(subtract(5,10))
+print(subtract.__doc__)
--- i.py
+++ i.py
@@ -1,5 +1,13 @@
-def inf():
-    print(spring)
-spring="autumn"
-inf()
+def greet(name):# here 'name' is parameter
+
+    print("Good Morning!!")
+    print("Hope you have a great day")
+    return name
+
+user_input=input("Enter name: ")
+print(greet(user_input))#call or invoke
+
+
+
+
 
