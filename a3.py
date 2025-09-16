2985d42bb62114b16409c46dfb5d13783c8a67b5
prateeksha-coder <prateeksha.codingal@gmail.com>
2025-09-16 13:30:05 +0530

    commit

--- a3.py
+++ a3.py
@@ -11,10 +11,24 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
+print("Please choose an option:")
+print("a. Addition")
+print("b. Subtraction")
+print("c. Product")
+print("d. Divide")
+
+choice=input("Enter your choice a/b/c/d: ")
+
 num1 = int(input("Enter Number 1 : "))
 num2 = int(input("Enter Number 2 : "))
 
-print("Sum :", add(num1, num2))
-print("Difference :", subtract(num1, num2))
-print("Product :", multiply(num1, num2))
-print("Quotient :", divide(num1, num2))
+if choice=='a':
+    print("Sum :", add(num1, num2))
+elif choice=='b':
+    print("Difference :", subtract(num1, num2))
+elif choice=='c':
+    print("Product :", multiply(num1, num2))
+elif choice=='d':
+    print("Quotient :", divide(num1, num2))
+else:
+    print("Invalid Input Choice!!")
